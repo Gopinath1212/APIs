dd8f5544b0d6c3e5c3fd71fac698454382c2b9d8
Gopinath1212 <130036208+Gopinath1212@users.noreply.github.com>
2023-06-15 10:38:37 +0530

    Update Inter.dart

--- lib/Inter.dart
+++ lib/Inter.dart
@@ -3,15 +3,18 @@ import 'dart:core';
 import 'List.dart';
 
 class Plans{
-  late List<Department> departments;
+   List<Department>? departments;
   Plans({
     required this.departments
   });
 
   factory Plans.fromJson(Map<String, dynamic> json) {
+
+    Iterable dep = json["departments"];
+
     return Plans(
         departments: json['departments']==null?null:
-        json['departments'].map((y)=>Plans.fromJson(y)).toList()
+        dep.map((y)=>Department.fromJson(y)).toList()
     );
   }
 }
